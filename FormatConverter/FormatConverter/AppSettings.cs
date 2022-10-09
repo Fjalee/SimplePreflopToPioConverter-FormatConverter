52cf4f99f178acbbe8197ab9e310f1f9dbc94523
Rytis Cepulis <cepRytis@gmail.com>
2022-10-10 01:02:21 +0300

    vsbet folders does not contian any information

--- FormatConverter/FormatConverter/AppSettings.cs
+++ FormatConverter/FormatConverter/AppSettings.cs
@@ -2,20 +2,21 @@
 {
     public class AppSettingsOptions
     {
-        public PositionNamesOptions? PositionNames { get; set; }
-        public string? InputDir { get; set; }
+        public PositionNamesOptions PositionNames { get; set; }
+        public string InputDir { get; set; }
+        public string RegexForVsBetFolder { get; set; }
     }
 
     public class PositionNamesOptions
     {
-        public string? SBName { get; set; }
-        public string? BBName { get; set; }
-        public string? UTGName { get; set; }
-        public string? MP1Name { get; set; }
-        public string? MP2Name { get; set; }
-        public string? MP3Name { get; set; }
-        public string? HIJName { get; set; }
-        public string? COName { get; set; }
-        public string? BTNName { get; set; }
+        public string SBName { get; set; }
+        public string BBName { get; set; }
+        public string UTGName { get; set; }
+        public string MP1Name { get; set; }
+        public string MP2Name { get; set; }
+        public string MP3Name { get; set; }
+        public string HIJName { get; set; }
+        public string COName { get; set; }
+        public string BTNName { get; set; }
     }
 }
--- FormatConverter/FormatConverter/MatchesTreeCreator.cs
+++ FormatConverter/FormatConverter/MatchesTreeCreator.cs
@@ -1,25 +1,59 @@
 ﻿using FormatConverter.TreeModel;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.RegularExpressions;
 
 namespace FormatConverter
 {
     public class MatchesTreeCreator : IMatchesTreeCreator
     {
-        private ILogger<MatchesTreeCreator> _logger;
-        private IPositionsMetaData _positionsMetaData;
+        private readonly ILogger<MatchesTreeCreator> _logger;
+        private readonly IPositionsMetaData _positionsMetaData;
+        private readonly AppSettingsOptions _config;
 
-        public MatchesTreeCreator(ILogger<MatchesTreeCreator> logger, IPositionsMetaData positionsMetaData)
+        public MatchesTreeCreator(ILogger<MatchesTreeCreator> logger,IPositionsMetaData positionsMetaData,
+            IOptions<AppSettingsOptions> configOptions)
         {
             _logger = logger;
             _positionsMetaData = positionsMetaData;
+            _config = configOptions.Value;
         }
 
         public MatchesTree Create(string inputDir)
         {
             var childDirs = GetInputDirectoryChildren(inputDir);
-
+            RemoveVsBetFolderFromDir(childDirs);
 
             return null;
+        }
+
+        private List<string> RemoveVsBetFolderFromDir(List<string> dirs)
+        {
+            var removedAtleastOne = false;
+
+            var result = new List<string>();
+            foreach (var dir in dirs)
+            {
+                var vsBetRegex = new Regex("\\\\" + _config.RegexForVsBetFolder);
+                var newDir = vsBetRegex.Replace(dir, "");
+                result.Add(newDir);
+
+                if (!removedAtleastOne)
+                {
+                    if (newDir != dir)
+                    {
+                        removedAtleastOne = true;
+                    }
+                }
+            }
+
+            if (!removedAtleastOne)
+            {
+                _logger.LogWarning("No vsbet folders were found (eg. 'vs_3bet')");
+            }
+
+            return result;
         }
 
         private List<string> GetInputDirectoryChildren(string inputDir)
