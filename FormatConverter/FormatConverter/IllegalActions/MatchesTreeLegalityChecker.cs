5ac41a3dc028592c84fadc9c1e0cc57562f4ff5f
cepRy <cepRytis@gmail.com>
2022-10-19 18:41:45 +0300

    for checking tree legality

--- FormatConverter/FormatConverter/IllegalActions/MatchesTreeLegalityChecker.cs
+++ FormatConverter/FormatConverter/IllegalActions/MatchesTreeLegalityChecker.cs
@@ -4,8 +4,36 @@ namespace FormatConverter.IllegalActions
 {
     public class MatchesTreeLegalityChecker : IMatchesTreeLegalityChecker
     {
-        public void ThrowIfIllegalMove(MatchesTreeNode tree, List<PositionEnum> positionsInUse)
+        private readonly ILegalityChecker _legalityChecker;
+
+        public MatchesTreeLegalityChecker(ILegalityChecker legalityChecker)
         {
+            _legalityChecker = legalityChecker;
+        }
+
+        public void ThrowIfIllegalMove(MatchesTreeNode root, List<PositionEnum> positionsInUse)
+        {
+            var currState = new MatchState(
+                positionsInUse.ToList().ToList(),
+                _legalityChecker.GetAvailableActionsAfterMove(TurnActionEnum.Raise, null),
+                PositionEnum.BB,
+                1.0,
+                positionsInUse.ToList());
+
+            RecursivelySimulateEveryTurn(root.ChildNodes, currState);
+        }
+
+        private void RecursivelySimulateEveryTurn(List<MatchesTreeNode> childNodes, MatchState currState)
+        {
+            foreach (var currChild in childNodes)
+            {
+                var cloneState = new MatchState(currState);
+                _legalityChecker.SimulateATurn(cloneState, currChild.Turn);
+                if (currChild.ChildNodes.Count > 0)
+                {
+                    RecursivelySimulateEveryTurn(currChild.ChildNodes, cloneState);
+                }
+            }
         }
     }
 }
