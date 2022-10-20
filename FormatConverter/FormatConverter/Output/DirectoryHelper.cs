ccc24c07b7a7b4e1c23acdbf660b9d846e54f900
cepRy <cepRytis@gmail.com>
2022-10-20 17:01:08 +0300

    output base dir

---
+++ FormatConverter/FormatConverter/Output/DirectoryHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace FormatConverter.Output
+{
+    public class DirectoryHelper
+    {
+        public void CreateDirThrowIfExists(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                throw new ArgumentException("Path already exists: " + path);
+            }
+
+            Directory.CreateDirectory(path);
+        }
+    }
+}
