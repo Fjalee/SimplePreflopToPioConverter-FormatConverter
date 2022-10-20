0b3674115b981391b248a66dad5ed10451a3e2c0
cepRy <cepRytis@gmail.com>
2022-10-20 16:56:12 +0300

    recursion to output all nodes

--- FormatConverter/FormatConverter/Output/C2Output.cs
+++ FormatConverter/FormatConverter/Output/C2Output.cs
@@ -6,6 +6,24 @@ namespace FormatConverter.Output
     {
         public void DoOutput(MatchesTreeNode treeRoot)
         {
+            RecursivelyOutputEveryNode(treeRoot.ChildNodes);
+        }
+
+        private void RecursivelyOutputEveryNode(List<MatchesTreeNode> childNodes)
+        {
+            foreach (var currChild in childNodes)
+            {
+                OutputNode();
+                if (currChild.ChildNodes.Count > 0)
+                {
+                    RecursivelyOutputEveryNode(currChild.ChildNodes);
+                }
+            }
+        }
+
+        private void OutputNode()
+        {
+
         }
     }
 }
