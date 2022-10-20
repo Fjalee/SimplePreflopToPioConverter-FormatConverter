2e288697087765a3e5792c4450e95311a30781ed
cepRy <cepRytis@gmail.com>
2022-10-20 08:58:10 +0300

    t

---
+++ FormatConverter/FormatConverter/InputFile/TurnBranchInPairs.cs
@@ -0,0 +1,14 @@
+﻿namespace FormatConverter.InputFile
+{
+    public class TurnBranchInPairs
+    {
+        public PlayerAndActionStringPair Pair { get; set; }
+        public string PathToFile { get; set; }
+
+        public TurnBranchInPairs(PlayerAndActionStringPair pair, string pathToFile)
+        {
+            Pair = pair;
+            PathToFile = pathToFile;
+        }
+    }
+}
